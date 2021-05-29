7e612c1b4813902b36815e8a370579c795cf0bc7
weharry95 <vihari95@outlook.com>
2021-05-29 17:07:33 -0400

    contactcard

--- contact-card-creator/src/tutorial/Handson_testing/data.js
+++
@@ -1,7 +0,0 @@
-export const data = [
-  { id: 1, name: 'Bertie Yates', age: '29 years' },
-  { id: 2, name: 'Hester Hogan', age: '32 years' },
-  { id: 3, name: 'Larry Little', age: '36 years' },
-  { id: 4, name: 'Lola Gardner', age: '29 years' },
-  { id: 4, name: 'Sean Walsh', age: '34 years' },
-]
